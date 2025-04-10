e52fabcbb9a695376c969609e4d54c9c2fbe3f6b
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-04-11 05:28:40 +0800

    Use custom smooth blit everywhere

--- src/main/java/thelm/techrebornjei/GuiRenderUtil.java
+++ src/main/java/thelm/techrebornjei/GuiRenderUtil.java
@@ -12,10 +12,37 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.resources.ResourceLocation;
 
 public class GuiRenderUtil {
 
-	public static void blitTiledSprite(PoseStack poseStack, TextureAtlasSprite sprite, int x, int y, float width, float height, int textureWidth, int textureHeight) {
+	public static void blit(PoseStack poseStack, ResourceLocation atlasLocation, float x, float y, float uOffset, float vOffset, float width, float height) {
+		blit(poseStack, atlasLocation, x, y, uOffset, vOffset, width, height, 256, 256);
+	}
+
+	public static void blit(PoseStack poseStack, ResourceLocation atlasLocation, float x, float y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight) {
+		float uMin = uOffset / textureWidth;
+		float uMax = (uOffset + width) / textureWidth;
+		float vMin = vOffset / textureHeight;
+		float vMax = (vOffset + height) / textureHeight;
+		blit(poseStack, atlasLocation, x, x + width, y, y + height, uMin, uMax, vMin, vMax);
+	}
+
+	public static void blitSprite(PoseStack poseStack, TextureAtlasSprite sprite, float x, float y, float uOffset, float vOffset, int textureWidth, int textureHeight) {
+		blitSprite(poseStack, sprite, x, y, uOffset, vOffset, textureWidth, textureHeight, textureWidth, textureHeight);
+	}
+
+	public static void blitSprite(PoseStack poseStack, TextureAtlasSprite sprite, float x, float y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight) {
+		float spriteWidth = sprite.getU1() - sprite.getU0();
+		float spriteHeight = sprite.getV1() - sprite.getV0();
+		float uMin = sprite.getU0() + uOffset / textureWidth * spriteWidth;
+		float uMax = sprite.getU0() + (uOffset + width) / textureWidth * spriteWidth;
+		float vMin = sprite.getV0() + vOffset / textureHeight * spriteHeight;
+		float vMax = sprite.getV0() + (vOffset + height) / textureHeight * spriteHeight;
+		blit(poseStack, sprite.atlasLocation(), x, x + width, y, y + height, uMin, uMax, vMin, vMax);
+	}
+
+	public static void blitTiledSprite(PoseStack poseStack, TextureAtlasSprite sprite, float x, float y, float width, float height, int textureWidth, int textureHeight) {
 		for(int i = 0; i < width; i += textureWidth) {
 			float drawWidth = Math.min(textureWidth, width - i);
 			for(int j = 0; j < height; j += textureHeight) {
@@ -25,26 +52,16 @@ public class GuiRenderUtil {
 		}
 	}
 
-	public static void blitSprite(PoseStack poseStack, TextureAtlasSprite sprite, int x, int y, float uOffset, float vOffset, int textureWidth, int textureHeight) {
-		blitSprite(poseStack, sprite, x, y, uOffset, vOffset, textureWidth, textureHeight, textureWidth, textureHeight);
-	}
-
-	public static void blitSprite(PoseStack poseStack, TextureAtlasSprite sprite, int x, int y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight) {
-		float spriteWidth = sprite.getU1() - sprite.getU0();
-		float spriteHeight = sprite.getV1() - sprite.getV0();
-		float uMin = sprite.getU0() + uOffset / textureWidth * spriteWidth;
-		float uMax = sprite.getU0() + (uOffset + width) / textureWidth * spriteWidth;
-		float vMin = sprite.getV0() + vOffset / textureHeight * spriteHeight;
-		float vMax = sprite.getV0() + (vOffset + height) / textureHeight * spriteHeight;
-		RenderSystem.setShaderTexture(0, sprite.atlasLocation());
+	static void blit(PoseStack poseStack, ResourceLocation atlasLocation, float xMin, float xMax, float yMin, float yMax, float uMin, float uMax, float vMin, float vMax) {
+		RenderSystem.setShaderTexture(0, atlasLocation);
 		RenderSystem.setShader(GameRenderer::getPositionTexShader);
 		Matrix4f matrix = poseStack.last().pose();
 		BufferBuilder bufferBuilder = Tesselator.getInstance().getBuilder();
 		bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
-		bufferBuilder.vertex(matrix, x, y, 0).uv(uMin, vMin).endVertex();
-		bufferBuilder.vertex(matrix, x, y + height, 0).uv(uMin, vMax).endVertex();
-		bufferBuilder.vertex(matrix, x + width, y + height, 0).uv(uMax, vMax).endVertex();
-		bufferBuilder.vertex(matrix, x + width, y, 0).uv(uMax, vMin).endVertex();
+		bufferBuilder.vertex(matrix, xMin, yMin, 0).uv(uMin, vMin).endVertex();
+		bufferBuilder.vertex(matrix, xMin, yMax, 0).uv(uMin, vMax).endVertex();
+		bufferBuilder.vertex(matrix, xMax, yMax, 0).uv(uMax, vMax).endVertex();
+		bufferBuilder.vertex(matrix, xMax, yMin, 0).uv(uMax, vMin).endVertex();
 		BufferUploader.drawWithShader(bufferBuilder.end());
 	}
 }
