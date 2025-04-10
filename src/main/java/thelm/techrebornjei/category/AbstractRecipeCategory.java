73a40a62d10ca71195b50d3c4127887869a9f209
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-04-11 05:28:26 +0800

    Use custom smooth blit everywhere

--- src/main/java/thelm/techrebornjei/category/AbstractRecipeCategory.java
+++ src/main/java/thelm/techrebornjei/category/AbstractRecipeCategory.java
@@ -5,7 +5,6 @@ import java.text.NumberFormat;
 import java.util.function.Supplier;
 
 import com.google.common.base.Suppliers;
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import mezz.jei.api.gui.drawable.IDrawable;
@@ -15,7 +14,6 @@ import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
-import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
@@ -23,9 +21,10 @@ import reborncore.client.gui.guibuilder.GuiBuilder;
 import reborncore.common.powerSystem.PowerSystem;
 import reborncore.common.powerSystem.PowerSystem.EnergySystem;
 import thelm.techrebornjei.EntryAnimation;
+import thelm.techrebornjei.GuiRenderUtil;
 import thelm.techrebornjei.TechRebornJEIPlugin;
 
-public abstract class AbstractRecipeCategory<R> extends GuiComponent implements IRecipeCategory<R> {
+public abstract class AbstractRecipeCategory<R> implements IRecipeCategory<R> {
 
 	public static final NumberFormat TIME_FORMAT = new DecimalFormat("###.##");
 
@@ -117,17 +116,16 @@ public abstract class AbstractRecipeCategory<R> extends GuiComponent implements 
 	public static final int PROGRESS_BAR_BREADTH = 10;
 
 	public void drawProgressBar(PoseStack poseStack, int x, int y, int animationDuration, GuiBuilder.ProgressDirection direction) {
-		RenderSystem.setShaderTexture(0, GuiBuilder.defaultTextureSheet);
-		blit(poseStack, x, y, direction.x, direction.y, direction.width, direction.height);
-		int drawLength = Math.round(System.currentTimeMillis() % animationDuration / (float)animationDuration * PROGRESS_BAR_LENGTH);
+		GuiRenderUtil.blit(poseStack, GuiBuilder.defaultTextureSheet, x, y, direction.x, direction.y, direction.width, direction.height);
+		float drawLength = System.currentTimeMillis() % animationDuration / (float)animationDuration * PROGRESS_BAR_LENGTH;
 		if(drawLength < 0) {
 			drawLength = 0;
 		}
 		switch(direction) {
-		case RIGHT -> blit(poseStack, x, y, direction.xActive, direction.yActive, drawLength, PROGRESS_BAR_BREADTH);
-		case LEFT -> blit(poseStack, x + PROGRESS_BAR_LENGTH - drawLength, y, direction.xActive + PROGRESS_BAR_LENGTH - drawLength, direction.yActive, drawLength, PROGRESS_BAR_BREADTH);
-		case UP -> blit(poseStack, x, y + PROGRESS_BAR_LENGTH - drawLength, direction.xActive, direction.yActive + PROGRESS_BAR_LENGTH - drawLength, PROGRESS_BAR_BREADTH, drawLength);
-		case DOWN -> blit(poseStack, x, y, direction.xActive, direction.yActive, PROGRESS_BAR_BREADTH, drawLength);
+		case RIGHT -> GuiRenderUtil.blit(poseStack, GuiBuilder.defaultTextureSheet, x, y, direction.xActive, direction.yActive, drawLength, PROGRESS_BAR_BREADTH);
+		case LEFT -> GuiRenderUtil.blit(poseStack, GuiBuilder.defaultTextureSheet, x + PROGRESS_BAR_LENGTH - drawLength, y, direction.xActive + PROGRESS_BAR_LENGTH - drawLength, direction.yActive, drawLength, PROGRESS_BAR_BREADTH);
+		case DOWN -> GuiRenderUtil.blit(poseStack, GuiBuilder.defaultTextureSheet, x, y, direction.xActive, direction.yActive, PROGRESS_BAR_BREADTH, drawLength);
+		case UP -> GuiRenderUtil.blit(poseStack, GuiBuilder.defaultTextureSheet, x, y + PROGRESS_BAR_LENGTH - drawLength, direction.xActive, direction.yActive + PROGRESS_BAR_LENGTH - drawLength, PROGRESS_BAR_BREADTH, drawLength);
 		}
 	}
 
@@ -138,11 +136,10 @@ public abstract class AbstractRecipeCategory<R> extends GuiComponent implements 
 		int innerWidth = ENERGY_DISPLAY_WIDTH - 2;
 		int innerHeight = ENERGY_DISPLAY_HEIGHT - 2;
 		EnergySystem displayPower = PowerSystem.getDisplayPower();
-		RenderSystem.setShaderTexture(0, GuiBuilder.defaultTextureSheet);
-		blit(poseStack, x, y, displayPower.xBar - ENERGY_DISPLAY_WIDTH - 1, displayPower.yBar - 1, ENERGY_DISPLAY_WIDTH, ENERGY_DISPLAY_HEIGHT);
-		int drawHeight;
+		GuiRenderUtil.blit(poseStack, GuiBuilder.defaultTextureSheet, x, y, displayPower.xBar - ENERGY_DISPLAY_WIDTH - 1, displayPower.yBar - 1, ENERGY_DISPLAY_WIDTH, ENERGY_DISPLAY_HEIGHT);
+		float drawHeight;
 		if(animation.animationType() != EntryAnimation.Type.NONE) {
-			drawHeight = Math.round(System.currentTimeMillis() % animation.duration() / (float)animation.duration() * innerHeight);
+			drawHeight = System.currentTimeMillis() % animation.duration() / (float)animation.duration() * innerHeight;
 			if(animation.animationType() == EntryAnimation.Type.DOWNWARDS) {
 				drawHeight = innerHeight - drawHeight;
 			}
@@ -150,7 +147,7 @@ public abstract class AbstractRecipeCategory<R> extends GuiComponent implements 
 		else {
 			drawHeight = innerHeight;
 		}
-		blit(poseStack, x + 1, y + innerHeight - drawHeight + 1, displayPower.xBar, innerHeight + displayPower.yBar - drawHeight, innerWidth, drawHeight);
+		GuiRenderUtil.blit(poseStack, GuiBuilder.defaultTextureSheet, x + 1, y + innerHeight - drawHeight + 1, displayPower.xBar, innerHeight + displayPower.yBar - drawHeight, innerWidth, drawHeight);
 	}
 
 	public boolean isInEnergyDisplay(int x, int y, double mouseX, double mouseY) {
