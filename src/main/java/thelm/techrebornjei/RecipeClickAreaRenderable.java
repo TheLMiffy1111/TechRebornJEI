040ca9588634438fa571a33e6cff7625e0428887
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-04-12 02:08:52 +0800

    Make fluid slot and JEI button renderer use drawables as well

--- src/main/java/thelm/techrebornjei/RecipeClickAreaRenderable.java
+++ src/main/java/thelm/techrebornjei/RecipeClickAreaRenderable.java
@@ -12,13 +12,18 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.screens.Screen;
 import reborncore.client.gui.builder.GuiBase;
+import reborncore.client.gui.guibuilder.GuiBuilder;
 import thelm.techrebornjei.mixin.ScreenAccessor;
 
-public record RecipeClickAreaRenderer(GuiBase<?> guiBase, int x, int y) implements Renderable {
+public record RecipeClickAreaRenderable(GuiBase<?> guiBase, int x, int y) implements Renderable {
+
+	public static final ResourceDrawable JEI_ICON = new ResourceDrawable(GuiBuilder.defaultTextureSheet, 202, 0, 12, 12);
 
 	@Override
 	public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
-		guiBase.builder.drawJEIButton(poseStack, guiBase, x, y, GuiBase.Layer.BACKGROUND);
+		if(!guiBase.hideGuiElements()) {
+			JEI_ICON.draw(poseStack, guiBase.getGuiLeft() + x, guiBase.getGuiTop() + y);
+		}
 	}
 
 	static final List<Entry> ENTRIES = new ArrayList<>();
@@ -40,14 +45,14 @@ public record RecipeClickAreaRenderer(GuiBase<?> guiBase, int x, int y) implemen
 	}
 
 	static {
-		ScreenEvents.AFTER_INIT.register(RecipeClickAreaRenderer::onAfterScreenInit);
+		ScreenEvents.AFTER_INIT.register(RecipeClickAreaRenderable::onAfterScreenInit);
 	}
 
 	static void onAfterScreenInit(Minecraft minecraft, Screen screen, int scaledWidth, int scaledHeight) {
 		if(screen instanceof GuiBase<?> guiBase) {
 			for(Entry entry : ENTRIES) {
 				if(entry.predicate.test(guiBase)) {
-					((ScreenAccessor)guiBase).trjei$addRenderable(new RecipeClickAreaRenderer(guiBase, entry.x, entry.y));
+					((ScreenAccessor)guiBase).trjei$addRenderable(new RecipeClickAreaRenderable(guiBase, entry.x, entry.y));
 					return;
 				}
 			}
