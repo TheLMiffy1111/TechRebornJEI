598026d2363719d7fa58b322b139398677800eff
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-04-11 16:47:25 +0800

    Make sprite drawables able to draw subsprites, and remove unnecessary UV parameters in draw sprite method

--- src/main/java/thelm/techrebornjei/GuiRenderUtil.java
+++ src/main/java/thelm/techrebornjei/GuiRenderUtil.java
@@ -27,8 +27,8 @@ public class GuiRenderUtil {
 		blit(poseStack, atlasLocation, x, x + width, y, y + height, uMin, uMax, vMin, vMax);
 	}
 
-	public static void blitSprite(PoseStack poseStack, TextureAtlasSprite sprite, float x, float y, float uOffset, float vOffset, int textureWidth, int textureHeight) {
-		blitSprite(poseStack, sprite, x, y, uOffset, vOffset, textureWidth, textureHeight, textureWidth, textureHeight);
+	public static void blitSprite(PoseStack poseStack, TextureAtlasSprite sprite, float x, float y, int textureWidth, int textureHeight) {
+		blitSprite(poseStack, sprite, x, y, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight);
 	}
 
 	public static void blitSprite(PoseStack poseStack, TextureAtlasSprite sprite, float x, float y, float uOffset, float vOffset, float width, float height, int textureWidth, int textureHeight) {
