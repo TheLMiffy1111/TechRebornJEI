1a616ef98165768261bdbd38a3d640727875bf5b
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-04-11 16:47:32 +0800

    Make sprite drawables able to draw subsprites, and remove unnecessary UV parameters in draw sprite method

--- src/main/java/thelm/techrebornjei/SpriteDrawable.java
+++ src/main/java/thelm/techrebornjei/SpriteDrawable.java
@@ -4,10 +4,13 @@ import java.util.function.Supplier;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
-import mezz.jei.api.gui.drawable.IDrawableStatic;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 
-public record SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int width, int height) implements IMaskableDrawable {
+public record SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int u, int v, int width, int height, int textureWidth, int textureHeight) implements IMaskableDrawable {
+
+	public SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int textureWidth, int textureHeight) {
+		this(spriteSupplier, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight);
+	}
 
 	@Override
 	public int getWidth() {
@@ -21,6 +24,6 @@ public record SpriteDrawable(Supplier<TextureAtlasSprite> spriteSupplier, int wi
 
 	@Override
 	public void draw(PoseStack poseStack, float xOffset, float yOffset, float maskTop, float maskBottom, float maskLeft, float maskRight) {
-		GuiRenderUtil.blitSprite(poseStack, spriteSupplier.get(), xOffset + maskLeft, yOffset + maskTop, maskLeft, maskTop, width - maskLeft - maskRight, height - maskTop - maskBottom, width, height);
+		GuiRenderUtil.blitSprite(poseStack, spriteSupplier.get(), xOffset + maskLeft, yOffset + maskTop, u + maskLeft, v + maskTop, width - maskLeft - maskRight, height - maskTop - maskBottom, textureWidth, textureHeight);
 	}
 }
