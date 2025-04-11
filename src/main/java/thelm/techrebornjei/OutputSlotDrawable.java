0cabba7cbf7894e37c6aa036285ad01d3d370bd0
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-04-11 16:19:13 +0800

    Directly use Reborn Core assets to draw slots

---
+++ src/main/java/thelm/techrebornjei/OutputSlotDrawable.java
@@ -0,0 +1,36 @@
+package thelm.techrebornjei;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import mezz.jei.api.gui.drawable.IDrawable;
+import reborncore.client.gui.guibuilder.GuiBuilder;
+
+public record OutputSlotDrawable(boolean left, boolean center, boolean right) implements IDrawable {
+
+	public static final IDrawable SLOT_BAR_LEFT = new ResourceDrawable(GuiBuilder.defaultTextureSheet, 150, 122, 3, 26);
+	public static final IDrawable SLOT_BAR_CENTER = new ResourceDrawable(GuiBuilder.defaultTextureSheet, 153, 122, 20, 26);
+	public static final IDrawable SLOT_BAR_RIGHT = new ResourceDrawable(GuiBuilder.defaultTextureSheet, 173, 122, 3, 26);
+
+	@Override
+	public int getWidth() {
+		return 26;
+	}
+
+	@Override
+	public int getHeight() {
+		return 26;
+	}
+
+	@Override
+	public void draw(PoseStack poseStack, int xOffset, int yOffset) {
+		if(left) {
+			SLOT_BAR_LEFT.draw(poseStack, xOffset, yOffset);
+		}
+		if(center) {
+			SLOT_BAR_CENTER.draw(poseStack, xOffset + 3, yOffset);
+		}
+		if(right) {
+			SLOT_BAR_RIGHT.draw(poseStack, xOffset + 23, yOffset);
+		}
+	}
+}
--- src/main/java/thelm/techrebornjei/category/ItemToItemRecipeCategory.java
+++ src/main/java/thelm/techrebornjei/category/ItemToItemRecipeCategory.java
@@ -24,8 +24,8 @@ public class ItemToItemRecipeCategory<R extends RebornRecipe> extends AbstractRe
 
 	@Override
 	public void setRecipe(IRecipeLayoutBuilder builder, R recipe, IFocusGroup focuses) {
-		builder.addSlot(RecipeIngredientRole.INPUT, 41, 20).addItemStacks(getInput(recipe, 0)).setBackground(standardSlot(), -1, -1);
-		builder.addSlot(RecipeIngredientRole.OUTPUT, 87, 20).addItemStack(getOutput(recipe, 0)).setBackground(outputSlot1(), -5, -5);
+		builder.addSlot(RecipeIngredientRole.INPUT, 41, 20).addItemStacks(getInput(recipe, 0)).setBackground(SLOT, -1, -1);
+		builder.addSlot(RecipeIngredientRole.OUTPUT, 87, 20).addItemStack(getOutput(recipe, 0)).setBackground(OUTPUT_SLOT, -5, -5);
 	}
 
 	@Override
