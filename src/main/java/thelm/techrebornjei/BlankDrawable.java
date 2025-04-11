e19b9ed297efe02e4032e51a894e9fb53df5b56e
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-04-11 16:33:23 +0800

    Make custom drawables supports float coordinates

--- src/main/java/thelm/techrebornjei/BlankDrawable.java
+++ src/main/java/thelm/techrebornjei/BlankDrawable.java
@@ -2,9 +2,7 @@ package thelm.techrebornjei;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 
-import mezz.jei.api.gui.drawable.IDrawableStatic;
-
-public record BlankDrawable(int width, int height) implements IDrawableStatic {
+public record BlankDrawable(int width, int height) implements IMaskableDrawable {
 
 	@Override
 	public int getWidth() {
@@ -17,8 +15,5 @@ public record BlankDrawable(int width, int height) implements IDrawableStatic {
 	}
 
 	@Override
-	public void draw(PoseStack poseStack, int xOffset, int yOffset) {}
-
-	@Override
-	public void draw(PoseStack poseStack, int xOffset, int yOffset, int maskTop, int maskBottom, int maskLeft, int maskRight) {}
+	public void draw(PoseStack poseStack, float xOffset, float yOffset, float maskTop, float maskBottom, float maskLeft, float maskRight) {}
 }
