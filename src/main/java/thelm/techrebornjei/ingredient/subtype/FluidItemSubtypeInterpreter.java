bf31ce879b7345568fbf179bd2d9dcf9bc803d30
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-08-03 23:37:17 +0800

    Make both charged and uncharged items show in ingredient list

---
+++ src/main/java/thelm/techrebornjei/ingredient/subtype/FluidItemSubtypeInterpreter.java
@@ -0,0 +1,18 @@
+package thelm.techrebornjei.ingredient.subtype;
+
+import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
+import mezz.jei.api.ingredients.subtypes.UidContext;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.item.ItemStack;
+import reborncore.common.fluid.container.ItemFluidInfo;
+
+public class FluidItemSubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack> {
+
+	@Override
+	public String apply(ItemStack ingredient, UidContext context) {
+		if(ingredient.getItem() instanceof ItemFluidInfo info) {
+			return BuiltInRegistries.FLUID.getKey(info.getFluid(ingredient)).toString();
+		}
+		return IIngredientSubtypeInterpreter.NONE;
+	}
+}
