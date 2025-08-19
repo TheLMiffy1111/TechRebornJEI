b07d15a735fcf3b8cfbe72db10f9f39b7762074f
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-08-19 14:26:29 +0800

    Update TechRebornJEI.java

--- src/main/java/thelm/techrebornjei/TechRebornJEI.java
+++ src/main/java/thelm/techrebornjei/TechRebornJEI.java
@@ -353,7 +353,7 @@ public class TechRebornJEI implements IModPlugin {
 
 		registration.addGenericGuiContainerHandler(GuiBase.class, new GuiBaseExtraAreaHandler());
 
-		RecipeClickAreaRenderable.clearEntries();;
+		RecipeClickAreaRenderable.clearEntries();
 
 		RecipeClickAreaRenderable.addEntry(GuiAlloyFurnace.class);
 		RecipeClickAreaRenderable.addEntry(GuiAlloySmelter.class);
